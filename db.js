4ea91b4ace06128aba9d3b253580b30a7e4519e7
taczaangel <angelroytaczaleon@gmail.com>
2025-11-07 11:23:28 -0500

    Cambiar a DATABASE_PUBLIC_URL para conexión BD

--- db.js
+++ db.js
@@ -1,16 +1,21 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:UCgDwgyEtVEONYWXDamtneFSUtmGuFYy@postgres.railway.internal:5432/railway',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  connectionString:
+    process.env.DATABASE_PUBLIC_URL ||
+    "postgresql://postgres:UCgDwgyEtVEONYWXDamtneFSUtmGuFYy@maglev.proxy.rlwy.net:37247/railway",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
-pool.on('connect', () => {
-  console.log('✅ Conexión a PostgreSQL establecida');
+pool.on("connect", () => {
+  console.log("✅ Conexión a PostgreSQL establecida");
 });
 
-pool.on('error', (err) => {
-  console.error('❌ Error en PostgreSQL:', err);
+pool.on("error", (err) => {
+  console.error("❌ Error en PostgreSQL:", err);
 });
 
 module.exports = pool;
